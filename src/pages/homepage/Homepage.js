0e4adbf9cad82c6aa8dc39068e8e233a9756054f
Albin Antony <albinantonydominic@gmail.com>
2023-07-19 20:34:04 +0530

    first commit

--- src/pages/homepage/Homepage.js
+++ src/pages/homepage/Homepage.js
@@ -1,4 +1,3 @@
-
 import { useContext, useEffect, useState } from "react";
 import "./Homepage.css";
 import axios from "axios";
@@ -15,30 +14,30 @@ const Homepage = () => {
   useEffect(() => {
     fetchData();
   }, []);
-  const {addItem} = useContext(Cartcontext);
+  const { addItem } = useContext(Cartcontext);
 
   return (
     <div>
-      <Navbar/>
+      <Navbar />
 
-    <div className="home">
-      {data.map((item, index) => {
-        item.quantity = 1;
-        return (
-          <div className="card" key={index}>
-            <div className="category">
+      <div className="home">
+        {data.map((item, index) => {
+          item.quantity = 1;
+          return (
+            <div className="card" key={index}>
+              <div className="category">
                 <div className="category-div">{item.category}</div>
               </div>
-            <img src={item.image} alt="" />
-            <p className="title">{item.title}</p>
-            <h3>$. {item.price}</h3>
-            <button className="add-to"onClick={() => addItem(item)}>
-              add to cart
-            </button>
-          </div>
-        );
-      })}
-    </div>
+              <img src={item.image} alt="" />
+              <p className="title">{item.title}</p>
+              <h3>$. {item.price}</h3>
+              <button className="add-to" onClick={() => addItem(item)}>
+                add to cart
+              </button>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- src/pages/routing-page/Contact.jsx
+++ src/pages/routing-page/Contact.jsx
@@ -1,55 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 const Contact = () => {
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [message, setMessage] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    // Perform form submission logic here
-    // You can access the form field values using the state variables (name, email, message)
-    // For example, you can send an HTTP request to a server or display a success message
-    console.log('Form submitted:', { name, email, message });
+
+    console.log("Form submitted:", { name, email, message });
   };
 
   return (
-    <div style={{display:'flex',justifyContent:'center',alignItems:'center',height:"100vh",gap:'3rem',flexDirection:'column'}}>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        gap: "3rem",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "3rem",
+          flexDirection: "column",
+        }}
+      >
+        <h1>Contact Us</h1>
+        <form
+          onSubmit={handleSubmit}
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "3rem",
+            flexDirection: "column",
+          }}
+        >
+          <div>
+            {" "}
+            <label htmlFor="name" style={{ marginRight: "15px" }}>
+              Name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+            />
+          </div>
 
-    <div style={{display:'flex',justifyContent:'center',alignItems:'center',gap:'3rem',flexDirection:'column'}} >
-      <h1>Contact Us</h1>
-      <form onSubmit={handleSubmit} style={{display:'flex',justifyContent:'center',alignItems:'center',gap:'3rem',flexDirection:'column'}} >
-        <div> <label htmlFor="name" style={{marginRight:"15px"}}>Name:</label>
-        <input
-          type="text"
-          id="name"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        /></div>
-       
-<div>
-<label htmlFor="email" style={{marginRight:"15px"}} >Email:</label>
-        <input
-          type="email"
-          id="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-</div>
-        
-<div>
-<label htmlFor="message" style={{marginRight:"15px"}}>Message:</label>
-        <textarea
-          id="message"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-</div>
-        
+          <div>
+            <label htmlFor="email" style={{ marginRight: "15px" }}>
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
 
-        <button type="submit">Submit</button>
-      </form>
-    </div>
+          <div>
+            <label htmlFor="message" style={{ marginRight: "15px" }}>
+              Message:
+            </label>
+            <textarea
+              id="message"
+              value={message}
+              onChange={(e) => setMessage(e.target.value)}
+            />
+          </div>
+
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     </div>
   );
 };
